68a95c71b01656bfc1535155132b2f84e0b39bb2
Danny <dannyjung23@gmail.com>
2024-07-17 22:01:59 +1200

    eatApple

--- snake.js
+++ snake.js
@@ -62,11 +62,11 @@ class Apple {
 
 var canvas = document.getElementById("canvas");
 
-var snake = new Snake();
+var snake = new Snake(20, 20, 20);
 
 var apple = new Apple();
 
-var canvasContext = canvas.getContext("2d");
+var canvasContext = canvas.getContext('2d');
 
 window.onload = () => {
     gameLoop();
@@ -82,7 +82,18 @@ function show() {
 }
 
 function update() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+    console.log("update")
+    snake.move()
+    eatApple()
+}
 
+function eatApple() {
+    if(snake.tail[snake.tail.length - 1].x == apple.x &&
+        snake.tail[snake.tail.length - 1].y == apple.y) {
+            snake.tail[snake.tail.length] = {x:apple.x, y:apple.y}
+            apple = new Apple();
+        }
 }
 
 function draw() {
@@ -100,6 +111,24 @@ function draw() {
 }
 
 function createRect(x, y, width, height, color) {
-    canvasContext.fillStyle = color;
+    canvasContext.fillStyle = color
     canvasContext.fillRect(x, y, width, height)
 }
+
+window.addEventListener("keydown", (event)=> {
+    setTimeout(()=> {
+        if(event.keyCode == 37 && snake.rotateX != 1) {
+            snake.rotateX = -1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 38 && snake.rotateY != 1) {
+            snake.rotateX = 0
+            snake.rotateY = -1;
+        } else if(event.keyCode == 39 && snake.rotateX != -1) {
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 40 && snake.rotateY != -1) {
+            snake.rotateX = 0
+            snake.rotateY = 1;
+        }
+    }, 1)
+})
